6fc90408ad442046e3616497b72d590de799225b
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-03-17 14:33:35 -0300

    transformando elemento ul em Componente
    deixando codigo mais limpo com componente Cards

---
+++ src/components/Gallery/Cards/index.jsx
@@ -0,0 +1,36 @@
+import heartIcon from './favorito.png'
+import openModal from './open.png'
+
+import React from 'react'
+
+function Cards({images, styles}) {
+  return (
+    <ul className={styles.gallery__cards}>
+        {
+          images.map(image => {
+            return (
+              <li key={image.id} className={styles.gallery__card}>
+                <img 
+                  className={styles.gallery__image}
+                  src={image.imagem} 
+                  alt={`imagem de ${image.titulo}`}
+                />
+
+                <p className={styles.gallery__description}>{image.titulo}</p>
+
+                <div>
+                  <p>{image.creditos}</p>
+                  <span>
+                    <img src={heartIcon} alt="icone coração para curtir imagem" />
+                    <img src={openModal} alt="icone de abrir modal" />
+                  </span>
+                </div>
+              </li>
+            )
+          })
+        }
+      </ul>
+  )
+}
+
+export default Cards
--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,43 +1,21 @@
 import styles from './gallery.module.scss'
-import heartIcon from './favorito.png'
-import openModal from './open.png'
 import images from './images.json'
 
 import React from 'react'
 import Tags from '../Tags'
+import Cards from './Cards'
 
-function Galery() {
+function Gallery() {
   return (
     <section className={styles.gallery}>
       <h2>Navegue pela Galería</h2>
       <Tags/>
-      <ul className={styles.gallery__cards}>
-        {
-          images.map(image => {
-            return (
-              <li key={image.id} className={styles.gallery__card}>
-                <img 
-                  className={styles.gallery__image}
-                  src={image.imagem} 
-                  alt={`imagem de ${image.titulo}`}
-                />
-
-                <p className={styles.gallery__description}>{image.titulo}</p>
-
-                <div>
-                  <p>{image.creditos}</p>
-                  <span>
-                    <img src={heartIcon} alt="icone coração para curtir imagem" />
-                    <img src={openModal} alt="icone de abrir modal" />
-                  </span>
-                </div>
-              </li>
-            )
-          })
-        }
-      </ul>
+      <Cards 
+        images={images}
+        styles={styles}
+      />
     </section>
   )
 }
 
-export default Galery
+export default Gallery
